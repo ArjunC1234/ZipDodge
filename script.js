5c307be6279806dcac16ff8e6d583e7a42af26c1
Glitch (glitch-hello-website) <none>
2022-11-09 16:53:13 +0000

    🏈🍫 Checkpoint ./script.js:31523611/2352

--- script.js
+++ script.js
@@ -1,46 +1,78 @@
+
+// VARIRABLES/OBJECTS/ETC. ---------------------------------------------------------------------------------------
+
+
 var canvas = document.getElementById("canv")
 var ctx = canvas.getContext("2d")
 
+canvas.width = 500
+canvas.height = 500
+//player object
 var player = {
   xvel : 0,
   yvel : 0,
   vector : [0, 0],
-  width : 50,
-  height : 50,
-  x : canvas.width/2 - 50/2,
-  y : canvas.height/2 - 50/2,
+  radius : 24,
+  x : canvas.width/2,
+  y : canvas.height/2,
   speed : 0,
-  maxspeed : 5,
+  maxspeed : 5
 }
 
+//mouse pos
 var m = {
   x : 0,
   y : 0
 }
 
+//fps data
+var frames = {
+  fps : 60
+}
+frames.interval = 1000/frames.fps
+frames.now = Date.now()
+frames.then = frames.now
+frames.delta = frames.now - frames.then
+
+
+// FUNCTIONS/EVENTS/ETC. ---------------------------------------------------------------------------------------
+
+//update mouse pos onclick
 canvas.addEventListener("mousedown", function (e) {
   let rect = canvas.getBoundingClientRect();
   m.x = event.clientX - rect.left;
   m.y = event.clientY - rect.top;
 })
 
-var now = 
-var then = now
-var delta = now - then
-var frames = {
-  now : Date.now(),
-  then : Date.now(),
-  delta : now - then,
-  fps : 60,
+//draw function
+function draw() {
+  ctx.beginPath();
+  ctx.arc(player.x, player.y, player.radius, 0, 2 * Math.PI, false);
+  ctx.fillStyle = 'cyan';
+  ctx.fill();
+  ctx.lineWidth = player.radius/10;
+  ctx.strokeStyle = 'pink';
+  ctx.stroke();
 }
 
-frames.interval = 1000/fps
+//vector calculator function
+function getVector
+
+
+//apply velocity
+function applyPlayerUpdates() 
+//game loop
 function loop() {
   requestAnimationFrame(loop)
   
   frames.now = Date.now()
-  
   frames.delta = frames.now - frames.then
   
-  
+  if (frames.delta >= frames.interval) {
+    draw()
+    
+    frames.then = frames.now - (frames.delta % frames.interval);
+  }
 }
+
+loop()
