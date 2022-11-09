719cde890804cbc3c7949d718ceb0ba98e02cea1
Glitch (glitch-hello-website) <none>
2022-11-09 16:41:44 +0000

    🌊⛹ Checkpoint ./index.html:31523611/3395 ./style.css:31523611/4227 ./script.js:31523611/2308

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+var canvas = document.getElementById("canv")
+var ctx = canvas.getContext("2d")
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+var player = {
+  xvel : 0,
+  yvel : 0,
+  vector : [0, 0],
+  width : 50,
+  height : 50,
+  x : canvas.width/2 - 50/2,
+  y : canvas.height/2 - 50/2,
+  speed : 0,
+  maxspeed : 5,
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+var m = {
+  x : 0,
+  y : 0
+}
+
+canvas.addEventListener("mousedown", function (e) {
+  let rect = canvas.getBoundingClientRect();
+  m.x = event.clientX - rect.left;
+  m.y = event.clientY - rect.top;
+})
+
+var now = 
+var then = now
+var delta = now - then
+var frames = {
+  now : Date.now(),
+  then : Date.now(),
+  delta : now - then,
+  fps : 60,
+}
+
+frames.interval = 1000/fps
+function loop() {
+  requestAnimationFrame(loop)
+  
+  frames.now = Date.now()
+  
+  frames.delta = frames.now - frames.then
+  
+  
+}
